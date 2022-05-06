c27f171016ed488da1f0b3c780609e746c828d1b
Adekanmi Daniel Omole <92954583+Ade-omole@users.noreply.github.com>
2022-05-06 21:41:15 +0200

    Update sto_SIR_Milstein.m

--- sto_SIR_Milstein.m
+++ sto_SIR_Milstein.m
@@ -15,14 +15,11 @@ for j = 1:L-1
 
     Winc = sum(dW(R*(j-1)+1:R*j));
 
-    S(j+1) = S(j) + (lambda-beta*S(j).*I(j)-mu*S(j))*Dt +...
-            sigma_1*S(j).*Winc + 0.5*sigma_1^2*S(j)*(Winc.^2 - Dt);
+    S(j+1) = S(j) + (lambda-beta*S(j).*I(j)-mu*S(j))*Dt + sigma_1*S(j).*Winc + 0.5*sigma_1^2*S(j)*(Winc.^2 - Dt);
 
-    I(j+1) = I(j) + (beta*I(j)-(mu+epsilon+gamma)*I(j))*Dt...
-            + sigma_2*I(j).*Winc+ 0.5*sigma_2^2*I(j).*(Winc.^2 - Dt);
+    I(j+1) = I(j) + (beta*I(j)-(mu+epsilon+gamma)*I(j))*Dt + sigma_2*I(j).*Winc+ 0.5*sigma_2^2*I(j).*(Winc.^2 - Dt);
 
-    R(j+1) = R(j) + (gamma*I(j)-mu*R(j))*Dt + sigma_3*R(j).*Winc...
-            + 0.5*sigma_3^2*R(j).*(Winc.^2 - Dt);
+    R(j+1) = R(j) + (gamma*I(j)-mu*R(j))*Dt + sigma_3*R(j).*Winc + 0.5*sigma_3^2*R(j).*(Winc.^2 - Dt);
 end
 
 figure(1)
